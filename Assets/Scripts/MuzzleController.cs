b2df3561c4db8f7df26033cae4ad749def65d8eb
Y3st5 <juandiex2005@gmail.com>
2026-04-28 19:23:47 -0500

    metodo de disparo de la torreta

--- Assets/Scripts/MuzzleController.cs
+++ Assets/Scripts/MuzzleController.cs
@@ -3,5 +3,62 @@ using UnityEngine.Events;
 using Sirenix.OdinInspector;
 public class MuzzleController : MonoBehaviour
 {
-    
+    public GameObject Enemy;
+    public GameObject BulletPrefab;
+    public GameObject headTurret;
+    public Transform FirePointRight;
+    public Transform FirePointLeft;
+    public bool aimMode;
+    public float rotationSpeed;
+    public float BulletSpeed = 20f;
+
+    private float fireRate = 1f;
+    private float nextFireTime = 0f;
+    private bool useRightFirePoint = true;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Detection();
+        Shoot();
+    }
+
+    public void Detection()
+    {
+        if (Enemy == null) return;
+
+        Vector3 HeadDir = (Enemy.transform.position - transform.position);
+
+        Quaternion targetQuaternion = Quaternion.LookRotation(HeadDir);
+        //transform.rotation = targetQuaternion;
+        headTurret.transform.rotation = Quaternion.Slerp(headTurret.transform.rotation, targetQuaternion, rotationSpeed * Time.deltaTime);
+
+        float distanceToEnemy = Vector3.Distance(transform.position, Enemy.transform.position);
+        if (distanceToEnemy > 15f) return;
+    }
+    public void Shoot()
+    {
+        if (Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+
+            Transform currentFirePoint = useRightFirePoint ? FirePointRight : FirePointLeft;
+            useRightFirePoint = !useRightFirePoint;
+
+            Debug.Log("Torreta Dispara");
+            GameObject bullet = Instantiate(BulletPrefab, currentFirePoint.position, currentFirePoint.rotation);
+
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.linearVelocity = currentFirePoint.forward * BulletSpeed;
+            }
+            Destroy(bullet, 3f);
+        }
+    }
 }
